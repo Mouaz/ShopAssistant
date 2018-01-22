39d517045b8318ddf7fd5c7f997c4c1637ffe443
Mouaz Alabsawi <mouaz.alabsawi@gmail.com>
2018-01-22 23:28:59 +0200

    angular and node web app

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var express = require('express');
+var app = express();
 var fs = require('fs');
 var NUMBER_OF_PRODUCTS = 26;//products +1
 var obj = JSON.parse(fs.readFileSync('data/data.json', 'utf8'));
@@ -9,26 +11,35 @@ var stdin = process.openStdin();
 
 //console.log(getProductBySubscriptionRange(10,100));
 
-stdin.addListener('data', function (d) {
+app.post('/chat', function (req, res) {
+    res.end(respondToMe('hi'));
+});
+app.get('/chat', function (req, res) {
+    res.end(respondToMe(req.query.msg));
+});
+var server = app.listen(8081, function () {
 
-    var input = d.toString().trim();
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("chatbot backend listening at http://%s:%s", host, port)
+
+})
+
+function respondToMe(input) {
     //console.log(obj['questionsAndAnswers']);
-
-    answered = false;
-
-
-    // console.log(qa);
-    // console.log(qa.q);
-    //console.log(current_type);
+    var input = input.toString().toLowerCase().trim();
+    var answered = false;
+    console.log(current_type);
     if (current_type != -1) {
 
         if (input.indexOf('search') > -1) {
             answer = obj['questionsAndAnswers'][3].a;
-            console.log(answer);
             message = 'user: ' + input + '\nbot: ' + answer + '\n';
             current_type = 3;
             writeToFileAsync(conversation_log_file, message);
             answered = true;
+            return answer;
         } else
             if (current_type == 3) {
                 answered = true;
@@ -39,18 +50,19 @@ stdin.addListener('data', function (d) {
                 s_r_arr = s_r_arr.concat(getProductByCategory(input, true));
                 search_results = new Set(s_r_arr);
                 answer = obj['questionsAndAnswers'][4].a + "\n" + s_r_arr;
-                console.log(obj['questionsAndAnswers'][4].a);
-                console.log(search_results);
+                // return obj['questionsAndAnswers'][4].a;
+                // return search_results;
                 message = 'user: ' + input + '\nbot: ' + answer + '\n';
                 writeToFileAsync(conversation_log_file, message);
+                return JSON.stringify(obj['questionsAndAnswers'][4].a)+'\n'+JSON.stringify(Array.from(search_results));
             }
             else if (input.indexOf('product') > -1) {
                 answer = obj['questionsAndAnswers'][3].a;
-                console.log(answer);
                 message = 'user: ' + input + '\nbot: ' + answer + '\n';
                 current_type = 4;
                 writeToFileAsync(conversation_log_file, message);
                 answered = true;
+                return answer;
             } else
                 if (current_type == 4) {
                     answered = true;
@@ -59,17 +71,18 @@ stdin.addListener('data', function (d) {
                     s_r_arr = s_r_arr.concat(getProductByName(input, true));
                     search_results = new Set(s_r_arr);
                     answer = obj['questionsAndAnswers'][4].a + "\n" + s_r_arr;
-                    console.log(obj['questionsAndAnswers'][4].a);
-                    console.log(search_results);
+                    // return obj['questionsAndAnswers'][4].a;
+                    // return search_results;
                     message = 'user: ' + input + '\nbot: ' + answer + '\n';
                     writeToFileAsync(conversation_log_file, message);
+                    return JSON.stringify(obj['questionsAndAnswers'][4].a)+'\n'+JSON.stringify(Array.from(search_results));
                 } else if (input.indexOf('brand') > -1) {
                     answer = obj['questionsAndAnswers'][3].a;
-                    console.log(answer);
                     message = 'user: ' + input + '\nbot: ' + answer + '\n';
                     current_type = 5;
                     writeToFileAsync(conversation_log_file, message);
                     answered = true;
+                    return answer;
                 } else
                     if (current_type == 5) {
                         answered = true;
@@ -78,17 +91,18 @@ stdin.addListener('data', function (d) {
                         s_r_arr = s_r_arr.concat(getProductByBrand(input, true));
                         search_results = new Set(s_r_arr);
                         answer = obj['questionsAndAnswers'][4].a + "\n" + s_r_arr;
-                        console.log(obj['questionsAndAnswers'][4].a);
-                        console.log(search_results);
+                        // return obj['questionsAndAnswers'][4].a;
+                        // return search_results;
                         message = 'user: ' + input + '\nbot: ' + answer + '\n';
                         writeToFileAsync(conversation_log_file, message);
+                        return JSON.stringify(obj['questionsAndAnswers'][4].a)+'\n'+JSON.stringify(Array.from(search_results));
                     } else if (input.indexOf('category') > -1) {
                         answer = obj['questionsAndAnswers'][3].a;
-                        console.log(answer);
                         message = 'user: ' + input + '\nbot: ' + answer + '\n';
                         current_type = 6;
                         writeToFileAsync(conversation_log_file, message);
                         answered = true;
+                        return answer;
                     } else
                         if (current_type == 6) {
                             answered = true;
@@ -97,17 +111,18 @@ stdin.addListener('data', function (d) {
                             s_r_arr = s_r_arr.concat(getProductByCategory(input, true));
                             search_results = new Set(s_r_arr);
                             answer = obj['questionsAndAnswers'][4].a + "\n" + s_r_arr;
-                            console.log(obj['questionsAndAnswers'][4].a);
-                            console.log(search_results);
+                            // return JSON.stringify(obj['questionsAndAnswers'][4].a);
+                            // return search_results;
                             message = 'user: ' + input + '\nbot: ' + answer + '\n';
                             writeToFileAsync(conversation_log_file, message);
+                            return JSON.stringify(obj['questionsAndAnswers'][4].a)+'\n'+JSON.stringify(Array.from(search_results));
                         } else if (input.indexOf('price') > -1) {
                             answer = obj['questionsAndAnswers'][5].a;
-                            console.log(answer);
                             message = 'user: ' + input + '\nbot: ' + answer + '\n';
                             current_type = 7;
                             writeToFileAsync(conversation_log_file, message);
                             answered = true;
+                            return answer;
                         } else
                             if (current_type == 7) {
                                 answered = true;
@@ -120,35 +135,42 @@ stdin.addListener('data', function (d) {
                                 console.log(search_results);
                                 message = 'user: ' + input + '\nbot: ' + answer + '\n';
                                 writeToFileAsync(conversation_log_file, message);
+                                return JSON.stringify(obj['questionsAndAnswers'][4].a)+'\n'+JSON.stringify(Array.from(search_results));
+                                
                             }
     } else
+        var answw = '';
         obj['questionsAndAnswers'].forEach(qa => {
+           
             if (qa.q.indexOf(input) > -1 && !answered) {
 
-
-
+                console.log(qa);
                 if (qa.type == 1) {
-                    console.log(qa.a);
                     answered = true;
                     greetings = true;
                     conversation_log_file = 'conversation_logs\\conv_' + new Date().toLocaleDateString() + '.log';
                     //+ '_' + new Date().toLocaleTimeString().replace(/[&\/\\#,+()$~%.'':*?<>{}]/g, '_') + '.log';
                     message = 'user: ' + input + '\nbot: ' + qa.a + '\n';
                     current_type = 1;
+                    answw= qa.a;
                     writeToFileAsync(conversation_log_file, message);
                 }
             }
         });
-
+        if(answw!=''){current_type = 1;
+        return answw;
+    }
+        console.log(current_type);
+        console.log(answered);
     i = getRandomInt(1, 3)
     if (!answered) {
         if (i % 2 == 0) {
-            console.log('Pardon my stupidity, try with different keywords ');
+            return 'Pardon my stupidity, try with different keywords ';
         }
         else
-            console.log('I don\'t get it, kindly say something different');
+            return 'I don\'t get it, kindly say something different';
         if (current_type == 2) {
-            console.log(obj['questionsAndAnswers'][0].a);
+            return obj['questionsAndAnswers'][0].a;
             answered = true;
             greetings = true;
             message = 'user: ' + input + '\nbot: ' + obj['questionsAndAnswers'][0].a + '\n';
@@ -156,8 +178,8 @@ stdin.addListener('data', function (d) {
             writeToFileAsync(conversation_log_file, message);
         }
     }
-});
 
+}
 /*
 products helper
 author: Mouaz
